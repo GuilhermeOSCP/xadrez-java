4d5edde2b81da446e946aa801b4a5ca87a613d22
GuilhermeOSCP <GuilhermeOSCP@gmail.com>
2021-07-19 09:50:16 -0300

    Primeira Classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-       System.out.println("Esse será um jogo de Xadrez!");
+       Position pos = new Position(3, 5);
+       System.out.println(pos);
+       
+       
 	}
 
 }
